c5422803e1e7a7ac16cec75944510c56d923427d
Andrea Grillo <grillo.andrea@iisgalvanimi.edu.it>
2025-02-26 12:57:05 +0000

    html e css

--- grillo_fila_b/src/app/app.component.ts
+++ grillo_fila_b/src/app/app.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SecondoComponent } from './secondo/secondo.component';
+import { TerzoComponent } from './terzo/terzo.component';
+import { QuartoComponent } from './quarto/quarto.component';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'],
+  imports: [SecondoComponent, TerzoComponent, QuartoComponent]
 })
 export class AppComponent {
   title = 'grillo_fila_b';
